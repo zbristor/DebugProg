456e0ec4a92c381dd23f29bd2f47f6881c7c219e
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 10:48:07 -0400

    first commit

---
+++ DebugProg.java
@@ -0,0 +1,13 @@
+
+public class DebugProg {
+		 public static void main(String[] args) {
+		 double x, y;
+		 
+		 x = 3.1415;
+		 y = 3.64;
+		 
+		 System.out.println("pi is approximately " + x);
+		 System.out.println("My GPA was " + y);
+		 }
+	}
+
